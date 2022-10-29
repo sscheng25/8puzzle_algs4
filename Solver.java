84287e8f8100549590a2df87f0226c7a262c28a0
Sisun Cheng <997723678@qq.com>
2022-10-29 18:24:24 -0400

    Update Solver.java

--- Solver.java
+++ Solver.java
@@ -4,6 +4,7 @@
  *  Description:
  **************************************************************************** */
 
+
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.StdOut;
@@ -11,14 +12,44 @@ import edu.princeton.cs.algs4.StdOut;
 public class Solver {
     private int move;
     private MinPQ pq;
+    private Searchnode init;
+
+    private class Searchnode implements Comparable<Searchnode> {
+        private Board board;
+        private int move;
+        private Searchnode pre;
+        private int manPriority;
+
+        public Searchnode(Board cur, int move, Searchnode p) {
+            board = cur;
+            this.move = move;
+            pre = p;
+            manPriority = cur.manhattan() + move;
+        }
+
+        public int compareTo(Searchnode that) {
+            return this.manPriority - that.manPriority;
+        }
+    }
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
         if (initial == null) {
             throw new IllegalArgumentException();
         }
+        /*
         pq = new MinPQ<>();
         pq.insert(initial);
+
+        Board temp = initial;
+        while (temp.manhattan() != 0) {
+            Iterable<Board> next = temp.neighbors();
+            next[0]
+        }
+        */
+        MinPQ<Searchnode> ques = new MinPQ<>();
+
+
     }
 
     // is the initial board solvable? (see below)
@@ -63,3 +94,4 @@ public class Solver {
     }
 
 }
+
